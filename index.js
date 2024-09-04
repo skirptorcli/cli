837881f829f5aff6a8ebdbd35a4057b2fb7ec41f
Rahul Tanwar <mr.rahultanwar@gmail.com>
2024-09-04 12:38:36 +0530

    feat: integrate logging functionality into the main execution flow

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const { program } = require("commander");
 const config_loader = require("./config_loader");
 const operation_loader = require("./operation_loader");
 const execution_engine = require("./execution_engine");
+const Logger = require("./utils/logger");
 
 async function main() {
   program
@@ -13,9 +14,16 @@ async function main() {
     .version("1.0.0")
     .argument("<config>", "Path to the configuration file")
     .action(async (config_path) => {
+      const universalTimeInMilliseconds = new Date().getTime();
+      const logger = new Logger({
+        logDir: config_path,
+        logToConsole: true,
+        logToFile: true,
+        logFileName: `${config_path}-${universalTimeInMilliseconds}.log`,
+      });
       try {
         // Load configuration
-        const config = await config_loader.load(config_path);
+        const config = await config_loader.load(config_path, logger);
 
         // Load operations
         const operations = await operation_loader.load_operations(
@@ -26,9 +34,9 @@ async function main() {
         const engine = new execution_engine(operations);
         await engine.execute(config);
 
-        console.log("Setup completed successfully!");
+        logger.info("Setup completed successfully!");
       } catch (error) {
-        console.error("Error during setup:", error.message);
+        logger.info("Error during setup:", error.message);
         process.exit(1);
       }
     });
